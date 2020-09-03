a74300c297742c6994a25d77f0ff5cc3b9462224
Jonathan Vieira Menezes <jonathanm@me.local>
2020-09-02 21:17:10 -0300

    Subindo tests do repository e add um novo projeto para BDD sem implementação

--- Devboost.DroneDelivery.UnitTestsTDD/API/AuthControllerTest.cs
+++ Devboost.DroneDelivery.UnitTestsTDD/API/AuthControllerTest.cs
@@ -11,7 +11,7 @@ using Xunit;
 
 namespace Devboost.DroneDelivery.Tests.API
 {
-    public class AuthControllerTest
+    public class DronesRepositoryTests
     {
         [Fact(DisplayName = "GetToken")]
         [Trait("AuthControllerTest", "Controller Tests")]
