d0d87f6bfb24914649936a4fc45679a6fbc942e9
Flavio Henrique <flavio.henriq@live.com>
2020-09-02 21:21:44 -0300

    Adicionado teste unitarios para as controllers

--- Devboost.DroneDelivery.Tests/API/EntregaControllerTest.cs
+++ Devboost.DroneDelivery.Tests/API/EntregaControllerTest.cs
@@ -3,7 +3,9 @@ using AutoMoqCore;
 using Devboost.DroneDelivery.Api.Controllers;
 using Devboost.DroneDelivery.Domain.Interfaces.Commands;
 using KellermanSoftware.CompareNetObjects;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -37,6 +39,58 @@ namespace Devboost.DroneDelivery.Tests.API
             var comparison = new CompareLogic();
             service.Verify(mock => mock.Inicia(), Times.Once());
             Assert.True(comparison.Compare(result, expectResponse).AreEqual);
+        }
+
+        [Fact]
+        public async void IniciaEntrega_test_BadRequest()
+        {
+            //Given
+            var mocker = new AutoMoqer();
+            var baseControllerMock = mocker.Create<EntregaController>();
+
+            var faker = AutoFaker.Create();
+
+            var response = false;
+
+            var responseTask = Task.Factory.StartNew(() => response);
+
+            var expectResponse = baseControllerMock.BadRequest("Entrega iniciada!");
+
+            var service = mocker.GetMock<IEntregaCommand>();
+            service.Setup(r => r.Inicia()).Returns(responseTask).Verifiable();
+
+            //When
+            var result = await baseControllerMock.IniciaEntrega();
+
+            //Then
+            //var comparison = new CompareLogic();
+            service.Verify(mock => mock.Inicia(), Times.Once());
+            Assert.True(((BadRequestResult)result).StatusCode == ((BadRequestObjectResult)expectResponse).StatusCode);
+        }
+
+        [Fact]
+        public async void IniciaEntrega_test_InternalServerError()
+        {
+            //Given
+            var mocker = new AutoMoqer();
+            var baseControllerMock = mocker.Create<EntregaController>();
+
+            var faker = AutoFaker.Create();
+
+            var responseTask = new Exception();
+
+            var expectResponse = baseControllerMock.StatusCode(500);
+
+            var service = mocker.GetMock<IEntregaCommand>();
+            service.Setup(r => r.Inicia()).ThrowsAsync(responseTask).Verifiable();
+
+            //When
+            var result = await baseControllerMock.IniciaEntrega();
+
+            //Then
+            //var comparison = new CompareLogic();
+            service.Verify(mock => mock.Inicia(), Times.Once());
+            Assert.True(((ObjectResult)result).StatusCode == expectResponse.StatusCode);
         }
     }
 }
--- Devboost.DroneDelivery.Tests/API/UsuarioControllerTest.cs
+++ Devboost.DroneDelivery.Tests/API/UsuarioControllerTest.cs
@@ -6,7 +6,9 @@ using Devboost.DroneDelivery.Domain.Interfaces.Commands;
 using Devboost.DroneDelivery.Domain.Interfaces.Queries;
 using Devboost.DroneDelivery.Domain.Params;
 using KellermanSoftware.CompareNetObjects;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
@@ -43,6 +45,62 @@ namespace Devboost.DroneDelivery.Tests.API
             var comparison = new CompareLogic();
             service.Verify(mock => mock.Criar(param), Times.Once());
             Assert.True(comparison.Compare(result, expectResponse).AreEqual);
+        }
+
+        [Fact]
+        public async void Cadastrar_test_BadRequest()
+        {
+            //Given
+            var mocker = new AutoMoqer();
+            var baseControllerMock = mocker.Create<UsuarioController>();
+
+            var faker = AutoFaker.Create();
+
+            var param = new UsuarioParam() { Nome = "", Senha = ""};
+
+            var response = false;
+
+            var responseTask = Task.Factory.StartNew(() => response);
+
+            var expectResponse = baseControllerMock.BadRequest("Usuário não cadastrado");
+
+            var service = mocker.GetMock<IUsuarioCommand>();
+            service.Setup(r => r.Criar(param)).Returns(responseTask).Verifiable();
+
+            //When
+            var result = await baseControllerMock.Cadastrar(param);
+
+            //Then
+            var comparison = new CompareLogic();
+            service.Verify(mock => mock.Criar(param), Times.Once());
+            Assert.True(comparison.Compare(result, expectResponse).AreEqual);
+        }
+
+        [Fact]
+        public async void Cadastrar_test_InternalServerError()
+        {
+            //Given
+            var mocker = new AutoMoqer();
+            var baseControllerMock = mocker.Create<UsuarioController>();
+
+            var faker = AutoFaker.Create();
+
+            var param = new UsuarioParam() { Nome = "", Senha = "" };
+
+            var responseTask = new Exception();
+
+            var expectResponse = baseControllerMock.StatusCode(500);
+
+            var service = mocker.GetMock<IUsuarioCommand>();
+            service.Setup(r => r.Criar(param)).ThrowsAsync(responseTask).Verifiable();
+
+            //When
+            var result = await baseControllerMock.Cadastrar(param);
+
+            //Then
+            var comparison = new CompareLogic();
+            service.Verify(mock => mock.Criar(param), Times.Once());
+            Assert.True(((ObjectResult)result).StatusCode == expectResponse.StatusCode);
         }
 
         [Fact(DisplayName = "GetAll")]
