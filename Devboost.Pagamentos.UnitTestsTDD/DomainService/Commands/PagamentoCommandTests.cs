716d84ccf31d264f857ab100f337dd2bb6b6d7c8
Robert Luiz <robert.tiweb@gmail.com>
2020-09-05 15:14:16 -0300

    Tests

---
+++ Devboost.Pagamentos.UnitTestsTDD/DomainService/Commands/PagamentoCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using AutoBogus;
+using Devboost.Pagamentos.Domain.Entities;
+using Devboost.Pagamentos.Domain.Params;
+using ServiceStack;
+using Xunit;
+
+namespace Devboost.Pagamentos.UnitTestsTDD.DomainService.Commands
+{
+    public class PagamentoCommandTests
+    {
+
+       
+    }
+}
