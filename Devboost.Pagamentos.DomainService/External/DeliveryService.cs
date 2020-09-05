29783d012f89cb89f6feca70f7a6b746d29a5be6
Robert Luiz <robert.tiweb@gmail.com>
2020-09-05 17:19:36 -0300

    Services

---
+++ Devboost.Pagamentos.DomainService/External/DeliveryService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Devboost.Pagamentos.Domain.DTOs;
+using Devboost.Pagamentos.Domain.Interfaces.External;
+
+namespace Devboost.Pagamentos.DomainService.External
+{
+    public class DeliveryService: IDeliveryService
+    {
+        public Task SinalizaStatusPagamento(GatewayDTO statusPagamento)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Devboost.Pagamentos.DomainService/External/GatewayService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Devboost.Pagamentos.Domain.DTOs;
+using Devboost.Pagamentos.Domain.Entities;
+using Devboost.Pagamentos.Domain.Interfaces.External;
+
+namespace Devboost.Pagamentos.DomainService.External
+{
+    public class GatewayService:IGatewayService
+    {
+        public Task<GatewayDTO> EfetuaPagamento(PagamentoEntity pagamento)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
