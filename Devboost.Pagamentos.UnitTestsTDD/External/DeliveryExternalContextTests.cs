8ab4d6bb496e6ca2489e4f7657214ca36ad8f3cb
Robert Luiz <robert.tiweb@gmail.com>
2020-09-08 22:21:03 -0300

    Tests 2

---
+++ Devboost.Pagamentos.UnitTestsTDD/External/DeliveryExternalContextTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using AutoBogus;
+using Devboost.Pagamentos.Domain.Params;
+using Devboost.Pagamentos.Domain.VO;
+using Devboost.Pagamentos.External.Context;
+using Flurl.Http.Testing;
+using Xunit;
+
+namespace Devboost.Pagamentos.UnitTestsTDD.External
+{
+    public class DeliveryExternalContextTests
+    {
+        [Fact(DisplayName = "AtualizaStatusPagamento")]
+        [Trait("DeliveryExternalContext", "ExternalContext Tests")]
+        public async Task AtualizaStatusPagamento_Test()
+        {
+            var baseExternalMock = new DeliveryExternalContext(new ExternalConfigVO
+            {
+                DeliveryUrl = "http://teste",
+                GatewayUrl = "http://teste"
+            });
+
+            var param = new AutoFaker<DeliveryExternalParam>().Generate();
+
+            using var httpTest = new HttpTest();
+            // arrange
+            httpTest.RespondWith();
+
+            // act
+            await baseExternalMock.AtualizaStatusPagamento(param);
+
+            // assert
+            httpTest.ShouldHaveCalled("http://teste/entrega/inicia/pedido")
+                .WithVerb(HttpMethod.Post)
+                .WithRequestJson(param)
+                .Times(1);
+        }
+    }
+}
---
+++ Devboost.Pagamentos.UnitTestsTDD/External/GatewayExternalContextTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using AutoBogus;
+using Devboost.Pagamentos.Domain.Enums;
+using Devboost.Pagamentos.Domain.Params;
+using Devboost.Pagamentos.Domain.VO;
+using Flurl.Http.Testing;
+using Devboost.Pagamentos.External.Context;
+using Xunit;
+
+namespace Devboost.Pagamentos.UnitTestsTDD.External
+{
+    public class GatewayExternalContextTests
+    {
+        [Fact(DisplayName = "EfetuarPagamentoCartao")]
+        [Trait("GatewayExternalContext", "ExternalContext Tests")]
+        public async Task EfetuarPagamentoCartao_Test()
+        {
+            var baseExternalMock = new GatewayExternalContext( new ExternalConfigVO
+            {
+                DeliveryUrl = "http://teste",
+                GatewayUrl = "http://teste"
+            });
+
+            var param = new AutoFaker<GatewayCartaoParam>().Generate();
+
+            using var httpTest = new HttpTest();
+            // arrange
+            httpTest.RespondWithJson(new { StatusPagamentoEnum = "Aprovado" });
+
+            // act
+            await baseExternalMock.EfetuarPagamentoCartao(param);
+
+            // assert
+            httpTest.ShouldHaveCalled("http://teste/payments")
+                .WithVerb(HttpMethod.Post)
+                .WithRequestJson(param)
+                .Times(1);
+        }
+    }
+}
