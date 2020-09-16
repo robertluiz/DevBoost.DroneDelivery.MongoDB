acba5b10f6a7d0c66cd74d807e6be6ec0a5aab7a
Robert Luiz <robert.tiweb@gmail.com>
2020-09-16 20:55:22 -0300

    Guid fix

--- Devboost.DroneDelivery.Mongo/PedidosRepository.cs
+++ Devboost.DroneDelivery.Mongo/PedidosRepository.cs
@@ -5,6 +5,7 @@ using Devboost.DroneDelivery.Domain.Entities;
 using Devboost.DroneDelivery.Domain.Enums;
 using Devboost.DroneDelivery.Domain.Interfaces.Repository;
 using Devboost.DroneDelivery.Repository.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using ServiceStack;
 
@@ -29,7 +30,9 @@ namespace Devboost.DroneDelivery.Mongo
 
         public async Task<PedidoEntity> GetByID(Guid pedidoId)
         {
-            var filter = _filterDef.Eq(_ => _.Id, pedidoId);
+            var bsonId = new BsonBinaryData(pedidoId);
+
+            var filter = _filterDef.Eq(_ => _.Id, bsonId);
 
             var p = (await _pedidolCollection.FindAsync(filter)).FirstOrDefault();
 
@@ -39,9 +42,9 @@ namespace Devboost.DroneDelivery.Mongo
 
         public async Task<List<PedidoEntity>> GetByDroneID(Guid droneId)
         {
+            var bsonId = new BsonBinaryData(droneId);
 
-
-            var filter = _filterDef.Eq(_ => _.DroneId, droneId);
+            var filter = _filterDef.Eq(_ => _.DroneId, bsonId);
 
             var p = (await _pedidolCollection.FindAsync(filter)).FirstOrDefault();
 
@@ -51,8 +54,9 @@ namespace Devboost.DroneDelivery.Mongo
 
         public async Task<List<PedidoEntity>> GetByDroneIDAndStatus(Guid droneId, PedidoStatus status)
         {
+            var bsonId = new BsonBinaryData(droneId);
 
-            var filter = _filterDef.And(_filterDef.Eq(_ => _.DroneId, droneId), _filterDef.Eq(_ => _.Status, status.ToString()));
+            var filter = _filterDef.And(_filterDef.Eq(_ => _.DroneId, bsonId), _filterDef.Eq(_ => _.Status, status.ToString()));
 
             var p = (await _pedidolCollection.FindAsync(filter)).ToList();
 
@@ -62,8 +66,9 @@ namespace Devboost.DroneDelivery.Mongo
 
         public async Task<PedidoEntity> GetSingleByDroneID(Guid droneId)
         {
+            var bsonId = new BsonBinaryData(droneId);
 
-            var filter = _filterDef.And(_filterDef.Eq(_ => _.DroneId, droneId), _filterDef.Eq(_ => _.Status, PedidoStatus.EmTransito.ToString()));
+            var filter = _filterDef.And(_filterDef.Eq(_ => _.DroneId, bsonId), _filterDef.Eq(_ => _.Status, PedidoStatus.EmTransito.ToString()));
 
             var p = (await _pedidolCollection.FindAsync(filter)).FirstOrDefault();
             
@@ -84,9 +89,9 @@ namespace Devboost.DroneDelivery.Mongo
         public async Task Atualizar(PedidoEntity pedido)
         {
             var model = pedido.ConvertTo<Pedido>();
+            var bsonId = new BsonBinaryData(pedido.Id);
 
-
-            var filter = _filterDef.Eq(_ => _.Id, pedido.Id);
+            var filter = _filterDef.Eq(_ => _.Id, bsonId);
             var options = new FindOneAndReplaceOptions<Pedido>
             {
                 IsUpsert = true,
